cb594007308f9734049111074c2ad69f92dc8a14
Artsiom Yahorau <a.yahorau@andersenlab.com>
2022-08-04 11:39:47 +0300

    Added ActivityMainScreen

---
+++ app/src/androidTest/java/app/com/mobileassignment/views/pageObjects/ActivityMainScreen.java
@@ -0,0 +1,26 @@
+package app.com.mobileassignment.views.pageObjects;
+
+
+import app.com.mobileassignment.R;
+
+public class ActivityMainScreen extends BaseScreen {
+
+    public static int getSearchElement() { return R.id.search; }
+
+    public static int getResultsSearch() {
+        return R.id.results;
+    }
+
+    public static int getInputPoint() {
+        return R.id.insert_point;
+    }
+
+    public static int getCitiesListElement() {
+        return R.id.citiesList;
+    }
+
+    public static int getCityName() {
+        return R.id.cityName;
+    }
+
+}
