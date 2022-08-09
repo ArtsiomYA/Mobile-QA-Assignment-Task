90cdae4cd818cbb09dfa4c23ed8dcf5e601c8a78
Artsiom Yahorau <a.yahorau@andersenlab.com>
2022-08-09 15:44:19 +0300

    Added TestData class with data of testing

---
+++ app/src/androidTest/java/app/com/mobileassignment/views/utils/TestData.java
@@ -0,0 +1,21 @@
+package app.com.mobileassignment.views.utils;
+
+public class TestData {
+
+    private static final String MINSK_CITY = "Minsk";
+    private static final String HOFKE_CITY = "t Hofke";
+    private static final String INVALID_CITY = "11112222333";
+
+    public static String getMinskCity() {
+        return MINSK_CITY;
+    }
+
+    public static String getHofkeCity() {
+        return HOFKE_CITY;
+    }
+
+    public static String getInvalidCity() {
+        return INVALID_CITY;
+    }
+
+}
