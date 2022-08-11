97bf4ce704214d7c9a19954c80519c14c32f1d39
Artsiom Yahorau <a.yahorau@andersenlab.com>
2022-08-11 21:07:23 +0300

    Added new data of testing

--- app/src/androidTest/java/app/com/mobileassignment/views/utils/TestData.java
+++ app/src/androidTest/java/app/com/mobileassignment/views/utils/TestData.java
@@ -6,7 +6,8 @@ public class TestData {
     private static final String HOFKE_CITY = "t Hofke";
     private static final String INVALID_CITY = "11112222333";
     private static final String VITEBSK_CYRILLIC_CITY = "Витебск";
-    private static final String TITLE_MAIN_PAGE = "Mobile Assignment";
+    private static final String TITLE_MAIN_SCREEN = "Mobile Assignment";
+    private static final String KIEWA_PAGE = "kiewa";
 
     public static String getMinskCity() {
         return MINSK_CITY;
@@ -24,8 +25,12 @@ public class TestData {
         return VITEBSK_CYRILLIC_CITY;
     }
 
-    public static String getTitleMainPage() {
-        return TITLE_MAIN_PAGE;
+    public static String getTitleMainScreen() {
+        return TITLE_MAIN_SCREEN;
+    }
+
+    public static String getKiewaPage() {
+        return KIEWA_PAGE;
     }
 
 }
