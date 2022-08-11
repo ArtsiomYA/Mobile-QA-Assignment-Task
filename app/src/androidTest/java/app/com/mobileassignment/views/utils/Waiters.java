5cd7b3e35e485425a1306a1f2d59f11b5e66e952
Artsiom Yahorau <a.yahorau@andersenlab.com>
2022-08-11 10:14:19 +0300

    Rewritten by waiter

--- app/src/androidTest/java/app/com/mobileassignment/views/utils/Waiters.java
+++ app/src/androidTest/java/app/com/mobileassignment/views/utils/Waiters.java
@@ -1,20 +1,26 @@
 package app.com.mobileassignment.views.utils;
 
 import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 import android.view.View;
 
+import androidx.test.espresso.PerformException;
 import androidx.test.espresso.UiController;
 import androidx.test.espresso.ViewAction;
+import androidx.test.espresso.util.HumanReadables;
+import androidx.test.espresso.util.TreeIterables;
 
 import org.hamcrest.Matcher;
+
+import java.util.concurrent.TimeoutException;
 
 public class Waiters {
 
     /**
      * Perform action of waiting for a specific time.
      */
-    public ViewAction waitFor(final long millis) {
+    public ViewAction waitId(final int viewId, final long millis) {
         return new ViewAction() {
             @Override
             public Matcher<View> getConstraints() {
@@ -23,12 +29,33 @@ public class Waiters {
 
             @Override
             public String getDescription() {
-                return "Wait for " + millis + " milliseconds.";
+                return  "wait for a specific view with id <" + viewId + "> during " + millis + " millis.";
             }
 
             @Override
             public void perform(UiController uiController, final View view) {
-                uiController.loopMainThreadForAtLeast(millis);
+                final long startTime = System.currentTimeMillis();
+                final long endTime = startTime + millis - 500;
+                final Matcher<View> viewMatcher = withId(viewId);
+
+                do {
+                    for (View child : TreeIterables.breadthFirstViewTraversal(view)) {
+                        // found view with required ID
+                        if (viewMatcher.matches(child)) {
+                            return;
+                        }
+                    }
+
+                    uiController.loopMainThreadForAtLeast(50);
+                }
+                while (System.currentTimeMillis() < endTime);
+
+                // timeout happens
+                throw new PerformException.Builder()
+                        .withActionDescription(this.getDescription())
+                        .withViewDescription(HumanReadables.describe(view))
+                        .withCause(new TimeoutException())
+                        .build();
             }
         };
     }
