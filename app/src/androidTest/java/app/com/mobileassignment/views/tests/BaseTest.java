681a616a518bc0c73b3abe57b5a55be716096c1c
Artsiom Yahorau <a.yahorau@andersenlab.com>
2022-08-17 19:52:07 +0300

    Update BaseTest class

--- app/src/androidTest/java/app/com/mobileassignment/views/tests/BaseTest.java
+++ app/src/androidTest/java/app/com/mobileassignment/views/tests/BaseTest.java
@@ -1,6 +1,9 @@
 package app.com.mobileassignment.views.tests;
 
+import android.content.Context;
+
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.platform.app.InstrumentationRegistry;
 
 import org.junit.After;
 import org.junit.Before;
@@ -14,6 +17,7 @@ public class BaseTest {
 
     protected ActivityMainScreen activityMainScreen;
     protected ActivityMapScreen activityMapScreen;
+    protected Context appContext;
 
     @Rule
     public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
@@ -23,6 +27,7 @@ public class BaseTest {
     public void setUp() {
         activityMainScreen = new ActivityMainScreen();
         activityMapScreen = new ActivityMapScreen();
+        appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
     }
 
     @After
